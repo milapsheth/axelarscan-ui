6ad5acfab53c9a61d89a7c389e6b9bacb2138322
Sirapop Na Ranong <n.r.sirapop@gmail.com>
2023-05-29 23:02:01 +0700

    feat: setup network graph data

--- components/dashboard/index.js
+++ components/dashboard/index.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react'
 import { useSelector, shallowEqual } from 'react-redux'
+import _ from 'lodash'
 
 import Metrics from './metrics'
 import { getInflation } from '../../lib/api/axelar'
 import { GMPStats, GMPTotalVolume } from '../../lib/api/gmp'
 import { transfersStats, transfersTotalVolume } from '../../lib/api/transfers'
-import { toArray } from '../../lib/utils'
+import { getChainData } from '../../lib/config'
+import { split, toArray, normalizeQuote } from '../../lib/utils'
 
 export default () => {
   const {
@@ -82,8 +84,8 @@ export default () => {
       const metrics = ['GMPStats', 'GMPTotalVolume', 'transfersStats', 'transfersTotalVolume']
 
       const getData = async () => {
-        setInterchainData(
-          Object.fromEntries(
+        if (chains_data) {
+          const interchainData = Object.fromEntries(
             await Promise.all(
               toArray(
                 metrics.map(m =>
@@ -112,11 +114,82 @@ export default () => {
               )
             )
           )
-        )
+
+          const {
+            messages,
+          } = { ...interchainData?.GMPStats }
+
+          const {
+            data,
+          } = { ...interchainData?.transfersStats }
+
+          interchainData.networkGraph = _.orderBy(
+            Object.entries(
+              _.groupBy(
+                _.concat(
+                  toArray(messages).flatMap(m =>
+                    toArray(m.source_chains).flatMap(s =>
+                      toArray(s.destination_chains).map(d => {
+                        const {
+                          num_txs,
+                          volume,
+                        } = { ...d }
+
+                        const source_chain = normalizeQuote(s.key)
+                        const destination_chain = normalizeQuote(d.key)
+
+                        return {
+                          id: toArray([source_chain, destination_chain]).join('_'),
+                          source_chain,
+                          destination_chain,
+                          num_txs,
+                          volume,
+                        }
+                      })
+                    )
+                  ),
+                  toArray(data).map(d => {
+                    const {
+                      id,
+                      source_chain,
+                      destination_chain,
+                      num_txs,
+                      volume,
+                    } = { ...d }
+
+                    return {
+                      id: toArray([source_chain, destination_chain]).join('_'),
+                      source_chain,
+                      destination_chain,
+                      num_txs,
+                      volume,
+                    }
+                  }),
+                )
+                .filter(d => getChainData(d.source_chain, chains_data) && getChainData(d.destination_chain, chains_data)),
+                'id',
+              )
+            )
+            .map(([k, v]) => {
+              return {
+                ..._.head(v),
+                id: k,
+                num_txs: _.sumBy(v, 'num_txs'),
+                volume: _.sumBy(v, 'volume'),
+              }
+            }),
+            ['num_txs'], ['desc'],
+          )
+
+          setInterchainData(interchainData)
+        }
       }
+
       getData()
+      const interval = setInterval(() => getData(), 60 * 1000)
+      return () => clearInterval(interval)
     },
-    [],
+    [chains_data],
   )
 
   return (
